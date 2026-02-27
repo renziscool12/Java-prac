b2d1a8532084eb610522bd34446a53452c8b9b09
Renziee <dogetheink@gmail.com>
2026-02-28 00:14:06 +0800

    Update and rename OrderSystem.java to PizzaClass.java

---
+++ PizzaOrder/PizzaClass.java
@@ -0,0 +1,91 @@
+package practice2;
+import java.lang.IllegalArgumentException;
+import java.util.*;; 
+
+public class Pizza {
+	//enum for pizza size
+	public enum PizzaSize {
+		SMALL,
+		MEDIUM,
+		LARGE,
+		XLARGE
+	}
+	
+	//enum for pizza type
+	public enum PizzaType {
+		PEPPERONI,
+		CHEESY,
+		HAWAIIAN,
+		NEAPOLITAN
+	}
+	
+	public enum Toppings {
+		BACON,
+		OLIVES,
+		SAUSAGE,
+		ONIONS,
+		NONE
+	}
+	//fields
+	PizzaSize size;
+	PizzaType type;
+	private List<Toppings> toppings;
+	private double price;
+	private int quantity;
+	
+	//constructor
+	public Pizza(PizzaSize size, PizzaType type, List<Toppings> toppings, int quantity) {
+		this.size = size;
+		this.type = type;
+		this.toppings = new ArrayList<>(toppings); //defense copy
+		this.quantity = quantity;
+		this.price = getPriceForPizza() * this.quantity;
+	}
+	
+	
+	//getter
+	// Make sure to calculate each pizza's price only once and add it to the total.
+	// Do not call getPriceForPizza() multiple times per pizza unless intended.
+	// Always initialize your total price before summing multiple pizzas to avoid inflation.
+	// always initialize another variable if you want total all prices of the items
+	public double getPriceForPizza() {
+		double totalPrice = 0;
+		switch(size) {
+		case SMALL: totalPrice += 45.2; break;
+		case MEDIUM: totalPrice += 49.3; break;
+		case LARGE: totalPrice += 54.5; break;
+		case XLARGE: totalPrice += 60.22; break;	
+		default:
+			throw new IllegalArgumentException("Invalid!");
+		}
+		
+		switch(type) {
+		case PEPPERONI: totalPrice += 55.3; break;
+		case CHEESY: totalPrice += 61.1; break;
+		case HAWAIIAN: totalPrice += 66.2; break;
+		case NEAPOLITAN: totalPrice += 70.7; break;
+		default:
+			throw new IllegalArgumentException("Invalid!");
+		}
+		
+		totalPrice += toppings.size() * 6;
+		return totalPrice * quantity;
+	}
+	
+	public double getPrice() { return this.price; }
+	public int getQuantity() { return this.quantity; }
+	
+	public static void Menu() {
+			System.out.println("---Pandita's Pizza---");
+			System.out.println("[1] Order Pizza");
+			System.out.println("[2] View Order");
+			System.out.println("[3] Cancel Order");
+			System.out.println("[4] Total orders");
+			System.out.println("[5] Exit");
+			System.out.print("Your choice: ");
+	}
+	
+	public String toString() {
+		return String.format("Size: %s Type: %s with %s | Price: %.2f",  size, type, toppings, price);
+	}
+}
